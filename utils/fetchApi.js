f359676f33e7f6802c9db7fa17d503cae5cd6ae5
Dh529885 <89321290+Dh529885@users.noreply.github.com>
2021-11-30 02:11:32 -0500

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "3d522557d6msh570a017fbfe633dp1d95a0jsna09f56371221",
+    },
+  });
+};
